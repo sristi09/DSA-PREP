8458006941f89bd36efd864fb3a0f14d9696abcb
sristi09 <75315978+sristi09@users.noreply.github.com>
2022-06-13 13:13:24 +0530

    Create Linear Search.cpp

---
+++ Array/Linear Search.cpp
@@ -0,0 +1,20 @@
+int main() {
+    int n;
+    cin >> n;
+    int a[10];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    //Linear search
+    int key,k;
+    cin>>key; //key
+    for(k=0;k<n;k++){
+        if(a[k]==key){
+            cout<<"Found at position: "<<k;
+            break;
+        }
+    }
+    if(k==n){
+        cout<<"Not found";
+    }
+}
