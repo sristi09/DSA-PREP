cd08c5e2e42fb578cdf3845762a6d3f2f38310db
sristi09 <75315978+sristi09@users.noreply.github.com>
2022-06-18 15:21:00 +0530

    Update Number-pair.cpp

--- Array/Number-pair.cpp
+++ Array/Number-pair.cpp
@@ -1,4 +1,4 @@
-//2 pointer helps reduce time complexity to O(n) from O(n2) only if array sorted
+//2 pointer helps reduce time complexity to O(n) from O(n2) only if array sorted and there are only unique values in array
 #include <iostream>
 using namespace std;
 int main() {
@@ -24,3 +24,27 @@ int main() {
         }
     }
 }
+
+//better approach Time Complexity = O(n*logn) ~ O(n)
+int getPairsCount(int arr[], int n, int k) {
+        // code here
+        sort(arr,arr+n);
+        sort(arr, arr + n);
+    int x = 0, c = 0, y, z;
+    for (int i = 0; i < n - 1; i++) {
+        x = k - arr[i];
+       
+        // Lower bound from i+1
+        int y = lower_bound(arr + i + 1,
+                            arr + n, x) - arr;
+         
+        // Upper bound from i+1
+        int z = upper_bound(arr + i + 1,
+                            arr + n, x) - arr;
+        c = c + z - y;
+    }
+    return c;
+    }
+
+
+//best approach is using map
