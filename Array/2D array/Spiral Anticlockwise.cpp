0a068fceb3f4a455efd16cd665c389d1fc70dada
sristi09 <75315978+sristi09@users.noreply.github.com>
2022-06-22 20:12:00 +0530

    Update Spiral Anticlockwise.cpp

--- Array/2D array/Spiral Anticlockwise.cpp
+++ Array/2D array/Spiral Anticlockwise.cpp
@@ -10,35 +10,38 @@ int main() {
 			cin >> a[i][j];
 		}
 	}
-	int sc=0;
-	int ec =n-1;
-	int sr =0;
-	int er=m-1;
-	while(sc<=ec && sr<=er) {
-		for(i=sr;i<=er;i++){
+	int dir =0;
+	int sr=0;   //start row
+	int er=m-1;  //end row
+	int sc=0; //start columm
+	int ec=n-1; //end column
+	while(sr<=er && sc<=ec) {
+		if(dir==0){
+			for(int i=sr;i<=er;i++){
 			cout<<a[i][sc]<<", ";
 		}
-		sc++;
-		for(i=sc;i<=ec;i++) {
-			cout<<a[er][i]<<", ";
+			sc++;
 		}
-		er--;
-		
-		for(i=er;i>=sr;i--) {
-			cout<<a[i][ec]<<", ";
-			
+		else if(dir==1){
+			for(int i=sc;i<=ec;i++){
+				cout<<a[er][i]<<", ";
+			}
+			er--;
 		}
-		ec--;
-		
-		 
-		for(i=ec;i>=sc;i--) {
-			cout<<a[i][sr]<<", ";
-		
+		else if(dir==2){
+			for(int i=er;i>=sr;i--){
+				cout<<a[i][ec]<<", ";
+			}
+			ec--;
 		}
-		sr++;
-		
+		else if(dir==3){
+			for(int i=ec;i>=sc;i--){
+				cout<<a[sr][i]<<", ";
+			}
+			sr++;
+		}
+		dir = (dir+1)%4;
 	}
 	cout<<"END";
 	return 0;
-
 }
